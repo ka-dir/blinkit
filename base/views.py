634a576c763100d775a1ba4cf814e0f69d7d4812
kad-ir <kadieyr@gmail.com>
2023-03-14 23:50:45 +0300

    full crud

---
+++ base/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
+
+
+# Create your views here.
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == "POST":
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room-form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        # print(request.POST)
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room-form.html', context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    return render(request, 'base/room.html', {'room': room})
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete_temps.html', {'obj': room})
